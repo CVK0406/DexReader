5956b561ad7ff9883d27aea4c036a1ca12538f56
tamhuynhsc2003 <tamhuynhsc.2003@gmail.com>
2024-05-31 13:59:34 +0700

    update code history_domain

---
+++ app/src/main/java/com/example/dexreader/history/domain/HistoryUpdateUseCase.kt
@@ -0,0 +1,44 @@
+package com.example.dexreader.history.domain
+
+import kotlinx.coroutines.CoroutineStart
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.NonCancellable
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import com.example.dexreader.core.util.ext.printStackTraceDebug
+import com.example.dexreader.core.util.ext.processLifecycleScope
+import com.example.dexreader.history.data.HistoryRepository
+import org.example.dexreader.parsers.model.Manga
+import org.example.dexreader.parsers.util.runCatchingCancellable
+import com.example.dexreader.reader.ui.ReaderState
+import javax.inject.Inject
+
+class HistoryUpdateUseCase @Inject constructor(
+	private val historyRepository: HistoryRepository,
+) {
+
+	suspend operator fun invoke(manga: Manga, readerState: ReaderState, percent: Float) {
+		historyRepository.addOrUpdate(
+			manga = manga,
+			chapterId = readerState.chapterId,
+			page = readerState.page,
+			scroll = readerState.scroll,
+			percent = percent,
+			force = false,
+		)
+	}
+
+	fun invokeAsync(
+		manga: Manga,
+		readerState: ReaderState,
+		percent: Float
+	) = processLifecycleScope.launch(Dispatchers.Default, CoroutineStart.ATOMIC) {
+		runCatchingCancellable {
+			withContext(NonCancellable) {
+				invoke(manga, readerState, percent)
+			}
+		}.onFailure {
+			it.printStackTraceDebug()
+		}
+	}
+}
